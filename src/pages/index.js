0951c8387a9fb79a5461931402166eb219342de9
akhildhiman <akhilblackmetal69@gmail.com>
2020-06-10 15:39:13 +0530

    remove link from home

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,9 @@ import Head from "../components/head"
 const IndexPage = () => {
   return (
     <Layout>
-      <Head title="Home"/>
+      <Head title="Home" />
       <h1>Hello.</h1>
       <p>I'm Rick, a full-stack developer living in Seattle, Washington.</p>
-      <p>
-        Need a developer? <a href="www.google.com"> Contact Me</a>
-      </p>
     </Layout>
   )
 }
